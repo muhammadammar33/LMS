1b6a6244b6daba3259ff978f63581cb2f9009997
JavTahir <javeria.tahir2003@gmail.com>
2024-05-19 17:18:41 +0500

    completed issue-3

--- routes/student.js
+++ routes/student.js
@@ -1,10 +1,48 @@
-var express=require("express");
-var router= express.Router();
+const Student = require("../models/student");
+const Class = require("../models/class");
+const Courses = require("../models/courses");
+var express = require("express");
+var router = express.Router();
 //GET Routes
-router.get('/',function(req,res,next){
-    res.send("Student Dashboard");
+router.get("/", function (req, res, next) {
+  res.send("Student Dashboard");
 });
 
+//get all classes and courses assigned to student
+router.get("/dashboard", async (req, res) => {
+  try {
+    const studentId = req.query.studentId; // taking studentId from query
+    if (!studentId) {
+      return res.status(400).json({ error: "Student ID is required" });
+    }
 
+    const student = await Student.findById(studentId).exec();
+    if (!student) {
+      return res.status(404).json({ error: "Student not found" });
+    }
 
-module.exports=router;
+    const classes = await Class.find({ "students.sid": studentId }) //getting a students's classes
+      .select("name department")
+      .exec();
+    console.log(classes);
+
+    const courses = await Courses.find({ "students.sid": studentId }) //getting a students's courses
+      .select("name department")
+      .exec();
+
+    res.json({
+      student: {
+        name: student.name,
+        rollno: student.rollno,
+        department: student.department,
+      },
+      classes,
+      courses,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+module.exports = router;
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,10 +1,43 @@
-var express=require("express");
-var router= express.Router();
-//GET Routes
-router.get('/',function(req,res,next){
-    res.send("Teacher Dashboard");
+const Class = require("../models/class");
+const Courses = require("../models/courses");
+const Teacher = require("../models/teacher");
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/dashboard", async (req, res) => {
+  try {
+    const teacherId = req.query.teacherId;
+    if (!teacherId) {
+      return res.status(400).json({ error: "Teacher ID is required" });
+    }
+    const teacher = await Teacher.findById(teacherId).exec();
+    if (!teacher) {
+      return res.status(404).send({ message: "Teacher not found" });
+    }
+
+    const courses = await Courses.find({ "teachers.tid": teacherId }) //getting a teacher's courses
+      .select("name department")
+      .exec();
+
+    const classes = await Class.find({ "teachers.tid": teacherId }) //getting a teachers classes
+      .select("name department")
+      .exec();
+
+    res.json({
+      teacher: {
+        id: teacher._id,
+        name: teacher.name,
+        designation: teacher.designation,
+        department: teacher.department,
+      },
+
+      courses,
+      classes,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
-
-
-module.exports=router;
+module.exports = router;
